b5d8ce0489f0cdd09f7800f9f479902d874fc4b1
Emre Kaan Seylim <seylimemre@gmail.com>
2021-02-05 20:30:30 +0300

    Blok Yok Etme Oyunu

---
+++ Assets/Scripts/PlayMusic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusic : MonoBehaviour
+{
+    static PlayMusic music = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (music != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            music = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SceneManagenment.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagenment : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void HomePage()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitApplication()
+    {
+        Application.Quit();
+    }
+
+    public void WinnerButton()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
